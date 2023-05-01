1ce47ab54027cc48ae9ea6a2c639e934317a3ddb
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-01 23:26:10 +0200

    add login function

--- client/app.js
+++ client/app.js
@@ -5,3 +5,22 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+
+// Global variables
+let userName = '';
+
+// Functions
+const login = (e) => {
+  e.preventDefault();
+  if (userNameInput.value.length > 0) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+    addMessageForm.classList.add('show');
+  } else {
+    alert('Please enter a username');
+  }
+};
+
+// Event listeners
+loginForm.addEventListener('submit', (e) => login(e));
